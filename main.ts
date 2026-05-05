7286b6f605d1288fbefb426afe2c2c313e188701
Mykhailo-Odynets <147391057+Mykhailo-Odynets@users.noreply.github.com>
2026-05-05 14:02:37 +0200

    new start

--- main.ts
+++ main.ts
@@ -1,122 +1,41 @@
-/**
- * Final Optimized Stepper Library for 28BYJ-48 (I2C / PCA9685)
- * Features: Speed Control, Degree Rotation, Distance Travel, Sync Dual Motors
- */
-//% color="#228b22" icon="\uf085" block="Stepper Plus"
-namespace StepperMotorPlus {
-    const PCA9685_ADDRESS = 0x40
-    const MODE1 = 0x00
-    const LED0_ON_L = 0x06
-    const PRESCALE = 0xFE
-
-    const STP_CHA_L = 2047
-    const STP_CHA_H = 4095
-
-    const STP_CHB_L = 1
-    const STP_CHB_H = 2047
-
-    const STP_CHC_L = 1023
-    const STP_CHC_H = 3071
-
-    const STP_CHD_L = 3071
-    const STP_CHD_H = 1023
-
-    let initialized = false
-
-    export enum Steppers {
-        //% block="STPM1"
-        STP1 = 0x1,
-        //% block="STPM2"
-        STP2 = 0x2
+namespace StepperCar {
+    export enum Motors {
+        M1,
+        M2,
+        Both
     }
-    function i2cwrite(addr: number, reg: number, value: number) {
-        let buf = pins.createBuffer(2)
-        buf[0] = reg
-        buf[1] = value
-        pins.i2cWriteBuffer(addr, buf)
-    }
-    function i2cread(addr: number, reg: number) {
-        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
-        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
-        return val;
-    }
-    function setPwm(channel: number, on: number, off: number): void {
-        if (channel < 0 || channel > 15)
-            return;
-        //serial.writeValue("ch", channel)
-        //serial.writeValue("on", on)
-        //serial.writeValue("off", off)
 
-        let buf = pins.createBuffer(5);
-        buf[0] = LED0_ON_L + 4 * channel;
-        buf[1] = on & 0xff;
-        buf[2] = (on >> 8) & 0xff;
-        buf[3] = off & 0xff;
-        buf[4] = (off >> 8) & 0xff;
-        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
-    }
-    function setStepper(index: number, dir: boolean): void {
-        if (index == 1) {
-            if (dir) {
-                setPwm(0, STP_CHA_L, STP_CHA_H);
-                setPwm(2, STP_CHB_L, STP_CHB_H);
-                setPwm(1, STP_CHC_L, STP_CHC_H);
-                setPwm(3, STP_CHD_L, STP_CHD_H);
-            } else {
-                setPwm(3, STP_CHA_L, STP_CHA_H);
-                setPwm(1, STP_CHB_L, STP_CHB_H);
-                setPwm(2, STP_CHC_L, STP_CHC_H);
-                setPwm(0, STP_CHD_L, STP_CHD_H);
-            }
-        } else {
-            if (dir) {
-                setPwm(4, STP_CHA_L, STP_CHA_H);
-                setPwm(6, STP_CHB_L, STP_CHB_H);
-                setPwm(5, STP_CHC_L, STP_CHC_H);
-                setPwm(7, STP_CHD_L, STP_CHD_H);
-            } else {
-                setPwm(7, STP_CHA_L, STP_CHA_H);
-                setPwm(5, STP_CHB_L, STP_CHB_H);
-                setPwm(6, STP_CHC_L, STP_CHC_H);
-                setPwm(4, STP_CHD_L, STP_CHD_H);
-            }
+    export function MotorStart(motor: Motors, direction: boolean) : void {
+        if (motor == Motors.M1 || motor == Motors.Both) {
+            PCAmotor.StepperStart(PCAmotor.Steppers.STPM1, direction);
+        }
+        if (motor == Motors.M2 || motor == Motors.Both) {
+            PCAmotor.StepperStart(PCAmotor.Steppers.STPM2, direction);
         }
     }
-    function initPCA9685(): void {
-        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
-        setFreq(50);
-        for (let idx = 0; idx < 16; idx++) {
-            setPwm(idx, 0, 0);
+
+    export function MotorStop(motor: Motors) : void {
+        if (motor == Motors.M1 || motor == Motors.Both) {
+            PCAmotor.StepperStop(PCAmotor.Steppers.STPM1);
         }
-        initialized = true
-    }
-    function setFreq(freq: number): void {
-        // Constrain the frequency
-        let prescaleval = 25000000;
-        prescaleval /= 4096;
-        prescaleval /= freq;
-        prescaleval -= 1;
-        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
-        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
-        let newmode = (oldmode & 0x7F) | 0x10; // sleep
-        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
-        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
-        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
-        control.waitMicros(5000);
-        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+        if (motor == Motors.M2 || motor == Motors.Both) {
+            PCAmotor.StepperStop(PCAmotor.Steppers.STPM2);
+        }
     }
 
-    /**
-     * Stepper run
-     * @param index Stepper; eg: PCAmotor.Steppers.STPM1
-     * @param dir is forward direction; eg: true
-    */
-    //% blockId=magicbit_stepper_start block="Stepper 28BYJ-48|%index|start"
-    //% weight=92
-    export function StepperStart(index: Steppers, dir: boolean = true): void {
-        if (!initialized) {
-            initPCA9685()
-        }
-        setStepper(index, dir);
+    // distance - in centimeters
+    // diameter - in milimeters
+    export function CarMove(distance: number, diameter: number) : void {
+        PCAmotor.StpCarMove(distance, diameter)
+    }
+
+    // degree - arc degree to rotate right
+    // wD - wheel diameter
+    // cD - car diameter (distance between wheels centers)
+    export function CarRotate(degree: number, wD: number, cD: number) : void {
+        const degreeToRotate = (degree * cD) / (2 * Math.PI * wD)
+
+        PCAmotor.StepperDegree(PCAmotor.Steppers.STPM1, degreeToRotate)
+        PCAmotor.StepperDegree(PCAmotor.Steppers.STPM2, -degreeToRotate)
     }
 }
